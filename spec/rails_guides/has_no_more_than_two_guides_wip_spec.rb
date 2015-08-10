9848ff2ea050c8dc37f8a2b3b43b2476fc7fa743
Justin Toniazzo <jutonz42@gmail.com>
2015-08-10 14:17:32 -0400

    Adds starting point to rails guides question

--- spec/rails_guides/has_no_more_than_two_guides_wip_spec.rb
+++ spec/rails_guides/has_no_more_than_two_guides_wip_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'Rails guides', type: :feature do
   it 'has no more than two guides wip' do
-    # < insert test here >
+    # Start at http://rubyonrails.org/
   end
 end
