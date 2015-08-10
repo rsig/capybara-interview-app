56b346f4eff6d22c98067c82bcdaf08430869698
Justin Toniazzo <jutonz42@gmail.com>
2015-08-10 14:03:07 -0400

    Adds Wikipedia question

---
+++ spec/wikipedia/lists_capybara_as_in_kingdom_animalia_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'Wikipedia', type: :feature do
+  it 'lists the Capybara as in kingdom animalia' do
+    # Start at https://en.wikipedia.org/wiki/Main_Page
+  end
+end
