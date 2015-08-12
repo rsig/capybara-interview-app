0c723c261e460e1aa27c31bfa576ba57cda06612
Justin Toniazzo <jutonz42@gmail.com>
2015-08-12 09:54:26 -0400

    Adds instructions

--- spec/rails_guides/has_no_more_than_two_guides_wip_spec.rb
+++ spec/rails_guides/has_no_more_than_two_guides_wip_spec.rb
@@ -3,5 +3,8 @@ require 'spec_helper'
 describe 'Rails guides', type: :feature do
   it 'has no more than two guides wip' do
     # Start at http://rubyonrails.org/
+    # 1. Click Documentation
+    # 2. Click Rails Guides
+    # 3. Assert that there are no more than two guides listed as 'Work in Progress'
   end
 end
--- spec/rspec_blog/has_rspec_in_each_blog_title_spec.rb
+++ spec/rspec_blog/has_rspec_in_each_blog_title_spec.rb
@@ -3,5 +3,7 @@ require 'spec_helper'
 describe 'The RSpec blog', type: :feature do
   it "has the string 'RSpec' in each blog title" do
     # Start at rspec.info
+    # 1. Click on blog
+    # 2. Assert that each blog title contains the string 'RSpec'
   end
 end
