a3b1863e41f610c264c47ab670dfa7d8efe3acf9
Saravanan Kannan <saravanank@rduljonny1.citrite.net>
2016-08-05 13:30:53 -0400

    adds SF url

--- spec/sharefile/login_spec.rb
+++ spec/sharefile/login_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'Sharefile', type: :feature do
   it 'allows you to login with correct credentials' do
+    # https://rs-dev-prod.sharefile.com/, rstrialsf@gmail.com, Citrix123
     # visit sharefile url
     # login with credentials
     # logout
